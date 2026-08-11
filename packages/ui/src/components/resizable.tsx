0f96cebdcb4d53926b357005681538095fb353e0
Augustin MORVAL <amorval@diametral.com>
2026-08-11 16:23:03 +0200

    style(ui): convert snippet, resizable, and scroll-area to plain CSS

--- packages/ui/src/components/resizable.tsx
+++ packages/ui/src/components/resizable.tsx
@@ -11,10 +11,7 @@ function ResizablePanelGroup({
   return (
     <ResizablePrimitive.Group
       data-slot="resizable-panel-group"
-      className={cn(
-        "flex h-full w-full aria-[orientation=vertical]:flex-col",
-        className
-      )}
+      className={cn("ds-resizable-panel-group h-full w-full", className)}
       {...props}
     />
   )
@@ -34,10 +31,7 @@ function ResizableHandle({
   return (
     <ResizablePrimitive.Separator
       data-slot="resizable-handle"
-      className={cn(
-        "relative flex w-px items-center justify-center bg-border ring-offset-background after:absolute after:inset-y-0 after:start-1/2 after:w-1 after:-translate-x-1/2 rtl:after:translate-x-1/2 focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-hidden aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:start-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 rtl:aria-[orientation=horizontal]:after:-translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90",
-        className
-      )}
+      className={cn("ds-resizable-handle", className)}
       {...props}
     >
       {withHandle && (
--- packages/ui/src/components/scroll-area.tsx
+++ packages/ui/src/components/scroll-area.tsx
@@ -10,12 +10,12 @@ function ScrollArea({
   return (
     <ScrollAreaPrimitive.Root
       data-slot="scroll-area"
-      className={cn("relative", className)}
+      className={cn("ds-scroll-area", className)}
       {...props}
     >
       <ScrollAreaPrimitive.Viewport
         data-slot="scroll-area-viewport"
-        className="size-full rounded-[inherit] transition-[color,box-shadow] outline-none focus-visible:ring-[3px] focus-visible:ring-ring/50 focus-visible:outline-1"
+        className="ds-scroll-area-viewport"
       >
         {children}
       </ScrollAreaPrimitive.Viewport>
@@ -35,15 +35,12 @@ function ScrollBar({
       data-slot="scroll-area-scrollbar"
       data-orientation={orientation}
       orientation={orientation}
-      className={cn(
-        "flex touch-none p-px transition-colors select-none data-horizontal:h-2.5 data-horizontal:flex-col data-horizontal:border-t data-horizontal:border-t-transparent data-vertical:h-full data-vertical:w-2.5 data-vertical:border-s data-vertical:border-s-transparent",
-        className
-      )}
+      className={cn("ds-scroll-area-scrollbar", className)}
       {...props}
     >
       <ScrollAreaPrimitive.Thumb
         data-slot="scroll-area-thumb"
-        className="relative flex-1 rounded-none bg-border"
+        className="ds-scroll-area-thumb"
       />
     </ScrollAreaPrimitive.Scrollbar>
   )
